20f7f0e62ff227942b7ebfa109e63c1606e45c1d
0xdmimaz <korvin.ambr@gmail.com>
2024-02-23 17:14:25 +0200

    refactoring - added an iteration to output

--- check_mm_balance.py
+++ check_mm_balance.py
@@ -3,9 +3,9 @@ import random
 import web3
 from web3 import Web3
 
-endpoint = input("Enter your RPC endpoint: ")   # https://foo.bar
+endpoint = input('Enter your RPC endpoint: ')   # https://foo.bar
 web3 = Web3(web3.HTTPProvider(endpoint))
-address = input("Enter your address: ")   # 0x.....
+address = input('Enter your address: ')   # 0x.....
 idle = [1, 2]
 
 
@@ -13,8 +13,9 @@ def get_balance(_address, _time):
     balance = web3.eth.get_balance(_address)
     random_idle = random.choice(_time)
     time.sleep(random_idle)
-    print(f'IDLE: {random_idle} - BALANCE: {balance}')
+    return random_idle, balance
 
 
 for i in range(2000000):
-    get_balance(address, idle)
+    random_idle, balance = get_balance(address, idle)
+    print(f"ITERATION: {i} - IDLE: {random_idle} - BALANCE: {balance}")
