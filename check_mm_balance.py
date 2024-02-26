bb06fca01d028824c4c69b4f93d77bb24839c515
0xdmimaz <korvin.ambr@gmail.com>
2024-02-26 22:36:07 +0200

    some logging improvements

--- check_mm_balance.py
+++ check_mm_balance.py
@@ -1,12 +1,15 @@
 import time
 import random
+from datetime import datetime
 import web3
 from web3 import Web3
 
 endpoint = input('Enter your RPC endpoint: ')   # https://foo.bar
 web3 = Web3(web3.HTTPProvider(endpoint))
 address = input('Enter your address: ')   # 0x.....
+repeat_count = int(input('Enter your repeat count: '))   # 10
 idle = [1, 2]
+file = open('log.txt', 'a')
 
 
 def get_balance(_address, _time):
@@ -16,6 +19,11 @@ def get_balance(_address, _time):
     return random_idle, balance
 
 
-for i in range(2000000):
+for i in range(repeat_count):
     random_idle, balance = get_balance(address, idle)
-    print(f"ITERATION: {i} - IDLE: {random_idle} - BALANCE: {balance}")
+    dt = datetime.now()
+    log_str = f"{i} - {dt} - {endpoint} - {address} - {random_idle} - {balance}"
+    file.write(f"{log_str} \n")
+    print(f"{log_str}")
+
+file.close()
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 simple script, designed to get balance for some Metamask wallet, for some RPC
 
 Works in a loop (2000000 retries hardcoded) with a random idle
+
+ALSO. You should create a log.txt file in a project root
